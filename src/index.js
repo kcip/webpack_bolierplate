562259951f6d6f7d25069c4746b85ff357c0df3c
scott pick <scottpick@scotts-MBP.lan>
2023-05-22 20:07:13 -0400

    webpack config files with html bolierplate

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import generateJOke from './generateJoke'
+import './styles/main.scss';
+import laughing from './assets/laughing.png';
+const laughImg = document.getElementById('laughImg');
+
+laughImg.src = laughing;
+
+console.log('hello!!!');
